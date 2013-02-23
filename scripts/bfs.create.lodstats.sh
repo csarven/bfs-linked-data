5720148bba6c1d88db4b30f4dd771d08f5a19204
Sarven Capadisli <info@csarven.ca>
2013-02-23 09:49:47 +0100

    Some cleanup

--- scripts/bfs.create.lodstats.sh
+++ scripts/bfs.create.lodstats.sh
@@ -8,8 +8,8 @@
 . $HOME/lodstats-env/bin/activate
 
 . ./bfs.config.sh
-cd "$data"import
-rm *stats*
+
+rm "$data"import/*stats*
 
 #lodstats -val "$data"import/graph.meta.nt > "$data"import/graph.meta.nt.stats.ttl
 #find "$data" -name "*[!Structure|prov].rdf" | while read i ; do file=$(basename "$i"); dataSetCode=${file%.*};
@@ -25,5 +25,5 @@ for i in "$data"import/*.nt ; do
 #sys     0m0.292s
 
 echo "Fixing URI for meta stats" ;
-find "$data"import/*stats.ttl -name "*[!Structure|prov]" | while read i ; do sed -ri 's/<file:\/\/\/data\/bfs-linked-data\/data\/import\/([^\.]*)\.nt/<http:\/\/bfs.270a.info\/dataset\/\1/g' "$i" ; done ;
+find "$data"import/*stats.ttl -name "*[!Structure|prov]" | while read i ; do sed -ri 's/<file:\/\/\/data\/'"$agency"'-linked-data\/data\/import\/([^\.]*)\.nt/<http:\/\/'"$agency"'.270a.info\/dataset\/\1/g' "$i" ; done ;
 
--- scripts/bfs.get.sh
+++ scripts/bfs.get.sh
@@ -7,7 +7,7 @@
 
 . ./bfs.config.sh
 
-rm "$data"prov.retrieval.rdf
+rm "$data""$agency".prov.retrieval.rdf
 
 echo '<?xml version="1.0" encoding="UTF-8"?>
 <rdf:RDF
@@ -16,7 +16,7 @@ echo '<?xml version="1.0" encoding="UTF-8"?>
     xmlns:dcterms="http://purl.org/dc/terms/"
     xmlns:prov="http://www.w3.org/ns/prov#"
     xmlns:foaf="http://xmlns.com/foaf/0.1/"
-    xmlns:sdmx="http://purl.org/linked-data/sdmx#">' > "$data"prov.retrieval.rdf ;
+    xmlns:sdmx="http://purl.org/linked-data/sdmx#">' > "$data""$agency".prov.retrieval.rdf ;
 
 sleep 1
         dtstart=$(date +"%Y-%m-%dT%H:%M:%SZ") ;
@@ -41,7 +41,7 @@ sleep 1
         <prov:generated rdf:resource="http://bfs.270a.info/data/CH1_RN+HCL_HGDE_HIST+1.0.xml"/>
         <rdfs:label xml:lang="en">Retrieved CH1_RN+HCL_HGDE_HIST+1.0</rdfs:label>
         <rdfs:comment xml:lang="en">CH1_RN+HCL_HGDE_HIST+1.0 retrieved from source and saved to local filesystem.</rdfs:comment>
-    </rdf:Description>' >> "$data"prov.retrieval.rdf ;
+    </rdf:Description>' >> "$data""$agency".prov.retrieval.rdf ;
 
 counter=1;
 while read i ;
@@ -75,10 +75,10 @@ sleep 1
             <prov:generated rdf:resource="http://bfs.270a.info/data/'$DataSetCode'.xml"/>
             <rdfs:label xml:lang="en">Retrieved '$DataSetCode'</rdfs:label>
             <rdfs:comment xml:lang="en">'$DataSetCode' retrieved from source and saved to local filesystem.</rdfs:comment>
-        </rdf:Description>' >> "$data"prov.retrieval.rdf ;
+        </rdf:Description>' >> "$data""$agency".prov.retrieval.rdf ;
 
         (( counter++ ));
 
     done < data.txt
 
-echo -e "\n</rdf:RDF>" >> "$data"prov.retrieval.rdf ;
+echo -e "\n</rdf:RDF>" >> "$data""$agency".prov.retrieval.rdf ;
